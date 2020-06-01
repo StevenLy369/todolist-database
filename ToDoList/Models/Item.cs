c00b64992ee787340741b7b70afc650a0d050b63
Steven Ly <1stevenly92@gmail.com>
2020-06-01 15:27:36 -0700

    finished delete category feature

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
+using System;
+
 namespace ToDoList.Models
 {
     public class Item
     {
+        public Item()
+        {
+            this.Categories = new HashSet<CategoryItem>();
+        }
+
         public int ItemId { get; set; }
         public string Description { get; set; }
-        public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
+        public bool Complete { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public ICollection<CategoryItem> Categories { get; }
     }
 }
